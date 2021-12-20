c4dfa5b8823d7e036bca70d707fcc94a772778d3
samuelakason <93771056+samuelakason@users.noreply.github.com>
2021-12-20 13:40:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
 			<section id="colorlib-hero" class="js-fullheight" data-section="home">
 				<div class="flexslider js-fullheight">
 					<ul class="slides">
-				   	<li style="background-image: url(images/39467143.jpg);">
+				   	<li style="background-image: url(images/39467143.png);">
 				   		<div class="overlay"></div>
 				   		<div class="container-fluid">
 				   			<div class="row">
